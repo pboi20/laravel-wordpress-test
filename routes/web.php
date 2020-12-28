81573cfa7f8c5d3290c9fd19a792104bf0e3e9ba
Patrick Boivin <pboivin@gmail.com>
2020-12-27 21:13:02 -0500

    Setup /posts route (wip)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/posts', function () {
+    $response = Http::get(env('WP_REST_URL') . '/posts');
+    return $response->json();
 });
